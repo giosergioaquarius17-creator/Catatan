7a1197f183503612b4cced02f38b912a636545ab
giosergioaquarius17-creator <giosergioaquarius17@gmail.com>
2026-03-01 03:13:02 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,23 @@
-self.addEventListener('install', (e) => {
+const CACHE_NAME = 'tugasku-pro-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3'
+];
+
+// Install Service Worker
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll(['index.html', 'manifest.json']);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
+// Fetching Assets
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
+    caches.match(e.request).then(response => {
       return response || fetch(e.request);
     })
   );
